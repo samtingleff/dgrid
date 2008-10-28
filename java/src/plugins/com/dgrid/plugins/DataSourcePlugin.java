1e7bfa28426419bf427f220b9c6777efd1b30883
Sam Tingleff <sam@structure28.com>
2008-10-27 22:54:42 -0700

    named data sources

--- java/src/plugins/com/dgrid/plugins/DataSourcePlugin.java
+++ java/src/plugins/com/dgrid/plugins/DataSourcePlugin.java
@@ -19,7 +19,7 @@ public class DataSourcePlugin extends BaseDGridPlugin {
 	private String jdbcPassword;
 
 	public String getDescription() {
-		return "Provides a data source";
+		return "Provides for data sources";
 	}
 
 	public boolean enabledByDefault() {
@@ -61,6 +61,20 @@ public class DataSourcePlugin extends BaseDGridPlugin {
 	public Connection getConnection() throws Exception {
 		return getConnection(jdbcDriverClass, this.jdbcUrl, this.jdbcUsername,
 				this.jdbcPassword);
+	}
+
+	public Connection getConnection(String name) throws Exception {
+		DGridClient gridClient = ((DGridPluginContext) super.context)
+				.getGridClient();
+		String driverClass = gridClient.getSetting("dataSource." + name
+				+ ".driverClass", "com.mysql.jdbc.Driver");
+		String url = gridClient.getSetting("dataSource." + name + ".url",
+				"jdbc:mysql://192.168.1.116/e20");
+		String username = gridClient.getSetting("dataSource." + name
+				+ ".username", "e20");
+		String password = gridClient.getSetting("dataSource." + name
+				+ ".password", "phpsux");
+		return getConnection(driverClass, url, username, password);
 	}
 
 	public Connection getConnection(String driverClass, String url,
