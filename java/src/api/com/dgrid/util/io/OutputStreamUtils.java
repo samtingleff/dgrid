8a527f9f3bf073f3bd88efdfbd95711ababea357
Sam Tingleff <sam@structure28.com>
2008-12-08 10:16:36 -0800

    refactored and added a couple of new stream-to-stream methods

--- java/src/api/com/dgrid/util/io/OutputStreamUtils.java
+++ java/src/api/com/dgrid/util/io/OutputStreamUtils.java
@@ -5,10 +5,12 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.io.PrintStream;
 import java.nio.channels.FileChannel;
 
 public class OutputStreamUtils {
+	public static final int DEFAULT_BUFFER_SIZE = 1024;
 
 	public static void writeStringToFile(String content, File dest)
 			throws IOException {
@@ -19,14 +21,24 @@ public class OutputStreamUtils {
 		fos.close();
 	}
 
+	public static void writeStreamToStream(InputStream in, OutputStream out)
+			throws IOException {
+		writeStreamToStream(in, out, DEFAULT_BUFFER_SIZE);
+	}
+
+	public static void writeStreamToStream(InputStream in, OutputStream out,
+			int bufferSize) throws IOException {
+		int read = 0;
+		byte[] buffer = new byte[bufferSize];
+		while ((read = in.read(buffer)) > 0) {
+			out.write(buffer, 0, read);
+		}
+	}
+
 	public static void writeStreamToFile(InputStream in, File dest)
 			throws IOException {
 		FileOutputStream fos = new FileOutputStream(dest);
-		int read = 0;
-		byte[] buffer = new byte[1024];
-		while ((read = in.read(buffer)) > 0) {
-			fos.write(buffer, 0, read);
-		}
+		writeStreamToStream(in, fos);
 		fos.close();
 	}
 
